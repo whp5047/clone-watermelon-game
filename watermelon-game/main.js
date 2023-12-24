7cb4f655123dc403c4b1fd9fc7b93bbd6be56c38
whp5047 <whp5047@gmail.com>
2023-12-24 15:26:16 +0900

    #1 Feat: 그리드추가

---
+++ watermelon-game/main.js
@@ -0,0 +1,42 @@
+import { Bodies, Engine, Render, Runner, World,} from "matter-js";
+
+const engine = Engine.create();
+const render = Render.create({
+  engine,
+  element: document.body,
+  options: {
+    wireframes: false,
+    background: "#F7F4C8",
+    width: 620,
+    height: 850,
+  }
+});
+
+const world = engine.world;
+const leftWall = Bodies.rectangle(15, 395, 30, 790, {
+  isStatic: true,
+  render: { fillStyle: '#E6B143' }
+});
+
+const rightWall = Bodies.rectangle(605, 395, 30, 790, {
+  isStatic: true,
+  render: { fillStyle: '#E6B143' }
+});
+
+const ground = Bodies.rectangle(310, 820, 620, 60, {
+  isStatic: true,
+  render: { fillStyle: '#E6B143' }
+});
+
+const topLine = Bodies.rectangle(310, 150, 620, 2, {
+  name : 'topLine',
+  isStatic: true,
+  isSensor :true,
+  render: { fillStyle: '#E6B143' }
+});
+
+World.add(world, [leftWall, rightWall, ground, topLine]);
+
+Render.run(render);
+Runner.run(engine);
+
