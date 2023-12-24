77dffed73289464496b66c2cb8e1c7bfee04af0f
whp5047 <whp5047@gmail.com>
2023-12-24 15:20:04 +0900

    Feat : 과일 정의

---
+++ watermelon-game/fruits.js
@@ -0,0 +1,46 @@
+export const FRUITS = [
+    {
+        name: "fruits/00_cherry",
+        radius: 33 / 2,
+    },
+    {
+        name: "fruits/01_strawberry",
+        radius: 48 / 2,
+    },
+    {
+        name: "fruits/02_grape",
+        radius: 61 / 2,
+    },
+    {
+        name: "fruits/03_gyool",
+        radius: 69 / 2,
+    },
+    {
+        name: "fruits/04_orange",
+        radius: 89 / 2,
+    },
+    {
+        name: "fruits/05_apple",
+        radius: 114 / 2,
+    },
+    {
+        name: "fruits/06_pear",
+        radius: 129 / 2,
+    },
+    {
+        name: "fruits/07_peach",
+        radius: 156 / 2,
+    },
+    {
+        name: "fruits/08_pineapple",
+        radius: 177 / 2,
+    },
+    {
+        name: "fruits/09_melon",
+        radius: 220 / 2,
+    },
+    {
+        name: "fruits/10_watermelon",
+        radius: 259 / 2,
+    },
+];
